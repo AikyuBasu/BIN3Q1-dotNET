a78e4f77ab8defa79cb8ee353bf97a724997de08
Alicia Boltryk <alicia.boltryk@student.vinci.be>
2023-11-22 12:33:59 +0100

    S9

---
+++ WpfEmployee/ViewModels/EmployeeModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfEmployee.Models;
+
+namespace WpfEmployee.ViewModels
+{
+    public class EmployeeModel
+    {
+        private readonly Employee _employee;
+
+        public EmployeeModel(Employee employee)
+        {
+            _employee = employee;
+        }
+
+        public DateTime? BirthDate
+        {
+            get { return _employee.BirthDate; }
+        }
+
+        public String FullName
+        {
+            get { return _employee.FirstName + " " + _employee.LastName; }
+        }
+
+        public String DisplayBirthDate
+        {
+            get { return _employee.BirthDate.ToString(); }
+        }
+    }
+
+    
+
+    
+}
---
+++ WpfEmployee/ViewModels/EmployeeVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfEmployee.Models;
+
+
+// tous les noms trouvées dans le binding view doivent se retrouver ici
+namespace WpfEmployee.ViewModels
+{
+    public class EmployeeVM
+    {
+        private List<EmployeeModel> _employeesList;
+        private NorthwindContext context = new NorthwindContext();
+
+        private List<EmployeeModel> loadEmployees()
+        {
+            List<EmployeeModel> localCollection = new List<EmployeeModel>();
+            foreach (var item in context.Employees)
+            {
+                localCollection.Add(new EmployeeModel(item));
+
+            }
+
+            return localCollection;
+
+        }
+
+        public List<EmployeeModel> EmployeesList
+        {
+            get {
+                //if (_legumesList == null) _legumesList = loadLegumes2();
+                //return _legumesList;
+                return _employeesList = _employeesList ?? loadEmployees();
+            }
+        }
+    }
+
+    
+}
